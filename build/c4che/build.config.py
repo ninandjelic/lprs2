99bb0d2f488c01c14097fdce7d40458f7ec98cfa
ninandjelic <85516044+ninandjelic@users.noreply.github.com>
2021-06-07 20:36:31 +0200

    Add files via upload

---
+++ build/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x2001400
+tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'c', 'tooldir': None, 'funs': None}, {'tool': 'gcc', 'tooldir': None, 'funs': None}, {'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'cxx', 'tooldir': None, 'funs': None}, {'tool': 'gxx', 'tooldir': None, 'funs': None}]
---
+++ build/prarie_idx4.h
@@ -0,0 +1,112 @@
+#ifndef PRARIE_IDX4_H
+#define PRARIE_IDX4_H
+
+#include <stdint.h>
+
+extern uint32_t palette[16];
+
+extern uint16_t king_badGuy1__w;
+extern uint16_t king_badGuy1__h;
+extern uint32_t king_badGuy1__p[];
+
+extern uint16_t king_badGuy2__w;
+extern uint16_t king_badGuy2__h;
+extern uint32_t king_badGuy2__p[];
+
+extern uint16_t king_bullet__w;
+extern uint16_t king_bullet__h;
+extern uint32_t king_bullet__p[];
+
+extern uint16_t king_dirt1__w;
+extern uint16_t king_dirt1__h;
+extern uint32_t king_dirt1__p[];
+
+extern uint16_t king_dirt2__w;
+extern uint16_t king_dirt2__h;
+extern uint32_t king_dirt2__p[];
+
+extern uint16_t king_dirt3__w;
+extern uint16_t king_dirt3__h;
+extern uint32_t king_dirt3__p[];
+
+extern uint16_t king_dirt4__w;
+extern uint16_t king_dirt4__h;
+extern uint32_t king_dirt4__p[];
+
+extern uint16_t king_dirt_border__w;
+extern uint16_t king_dirt_border__h;
+extern uint32_t king_dirt_border__p[];
+
+extern uint16_t king_dirt_bush__w;
+extern uint16_t king_dirt_bush__h;
+extern uint32_t king_dirt_bush__p[];
+
+extern uint16_t king_down1__w;
+extern uint16_t king_down1__h;
+extern uint32_t king_down1__p[];
+
+extern uint16_t king_down2__w;
+extern uint16_t king_down2__h;
+extern uint32_t king_down2__p[];
+
+extern uint16_t king_down3__w;
+extern uint16_t king_down3__h;
+extern uint32_t king_down3__p[];
+
+extern uint16_t king_end__w;
+extern uint16_t king_end__h;
+extern uint32_t king_end__p[];
+
+extern uint16_t king_gameOver__w;
+extern uint16_t king_gameOver__h;
+extern uint32_t king_gameOver__p[];
+
+extern uint16_t king_hello__w;
+extern uint16_t king_hello__h;
+extern uint32_t king_hello__p[];
+
+extern uint16_t king_left1__w;
+extern uint16_t king_left1__h;
+extern uint32_t king_left1__p[];
+
+extern uint16_t king_left2__w;
+extern uint16_t king_left2__h;
+extern uint32_t king_left2__p[];
+
+extern uint16_t king_left3__w;
+extern uint16_t king_left3__h;
+extern uint32_t king_left3__p[];
+
+extern uint16_t king_right1__w;
+extern uint16_t king_right1__h;
+extern uint32_t king_right1__p[];
+
+extern uint16_t king_right2__w;
+extern uint16_t king_right2__h;
+extern uint32_t king_right2__p[];
+
+extern uint16_t king_right3__w;
+extern uint16_t king_right3__h;
+extern uint32_t king_right3__p[];
+
+extern uint16_t king_stand__w;
+extern uint16_t king_stand__h;
+extern uint32_t king_stand__p[];
+
+extern uint16_t king_title__w;
+extern uint16_t king_title__h;
+extern uint32_t king_title__p[];
+
+extern uint16_t king_up1__w;
+extern uint16_t king_up1__h;
+extern uint32_t king_up1__p[];
+
+extern uint16_t king_up2__w;
+extern uint16_t king_up2__h;
+extern uint32_t king_up2__p[];
+
+extern uint16_t king_up3__w;
+extern uint16_t king_up3__h;
+extern uint32_t king_up3__p[];
+
+#endif // PRARIE_IDX4_H
